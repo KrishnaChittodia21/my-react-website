f2cc35ba74c08171da9b8426afe7e764c66bc8fc
krishna <krishna.chittodia@successive.tech>
2020-07-01 03:51:42 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import PageWrapper from "./components/PageWrapper"
+
+//Pages
 import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
 
 function App() {
   return (
-    <PageWrapper>
-      <Home/>
-    </PageWrapper>
+    <Router>
+      <PageWrapper>
+        <Route
+          exact={true}
+          path="/"
+          component={Home}
+        />
+        <Route
+          path="/about"
+          component={About}
+        />
+      </PageWrapper>
+    </Router>
   );
 }
 
