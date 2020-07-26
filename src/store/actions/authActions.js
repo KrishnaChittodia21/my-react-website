948111b8df2acf458da9d7f7d0a3bb41a45324a8
krishna <krishnachittodia.10@gmail.com>
2020-07-27 02:32:21 +0530

    removed commented code

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -14,10 +14,6 @@ export const login = (email, pass) => {
       })
     })
   }
-  // return {
-  //   type: 'LOGIN',
-  //   payload: { email, pass}
-  // }
 }
 
 export const register = (email, pass) => {
