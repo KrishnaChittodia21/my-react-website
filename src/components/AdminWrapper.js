2dddfabba97bd02663b05b7f66314dbc92f47493
krishna <krishnachittodia.10@gmail.com>
2020-07-29 00:33:00 +0530

    initialised material ui icons and drawer to admin wrapper

--- src/components/AdminWrapper.js
+++ src/components/AdminWrapper.js
@@ -1,15 +1,82 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import "./assets/css/admin.css";
+
+// import classNames from 'classNames';
+import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-class AdminWrapper extends Component {
 
+//Drawer imports
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  toolbar: {
+    paddingRight: 24
+  },
+  appBar: {
+    zIndex: theme.zIndex + 1,
+    transitions: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width:`calc(100% - ${drawerWidth}px)`,
+    transitions: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'noWrap',
+    width: drawerWidth
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar
+  }
+})
+class AdminWrapper extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      open: true
+    }
+  }
+
+
+  handleDrawerOpen = (e) => {
+    this.setState({open:true});
+  }
+
+  handleDrawerClose = (e) => {
+    this.setState({open:false});
+  }
   render() {
+    const { classes } = this.props;
     return (
       <div id="admin-page">
-        <AppBar>
-          <Toolbar>
+        <AppBar className={classNames(classes.appBar, this.state.open && classes.appBarShift)}>
+          <Toolbar className={classes.toolbar}>
+            <IconButton>
+              <MenuIcon/>
+            </IconButton>
             <Typography
               component="h1"
               variant="h6"
@@ -18,10 +85,29 @@ class AdminWrapper extends Component {
             >Admin</Typography>
           </Toolbar>
         </AppBar>
+        <Drawer
+          variant="permanent"
+          open="true"
+          classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          <div className={classes.toolbarIcon}>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon/>
+            </IconButton>
+          </div>
+          <Divider/>
+          <List>
+            <ListItem>
+              Dashboard
+            </ListItem>
+          </List>
+        </Drawer>
         {this.props.children}
       </div>
     )
   }
 }
 
-export default AdminWrapper;
+export default withStyles(styles)(AdminWrapper);
